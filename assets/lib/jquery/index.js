1c5fd5503054ebc1712f88afae1d59a56719a741
Guotao H <wallfacerlogic@outlook.com>
2018-08-29 10:48:27 +0800

    Update index.js

--- assets/lib/jquery/index.js
+++ assets/lib/jquery/index.js
@@ -8,12 +8,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/favicon/bug.ico");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         document.title = '(!´･ω･`)! 页面崩溃啦 ~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         document.title = 'Δ(・ω・Δ) 噫又好了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
